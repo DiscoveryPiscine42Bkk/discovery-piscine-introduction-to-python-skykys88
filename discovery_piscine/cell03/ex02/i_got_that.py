2954de536ad628c14ec78dbbaddc1de4d2dc2e62
Sippapas Thienwiwat (SKYKYS) <128138867+skykys88@users.noreply.github.com>
2025-11-05 15:48:14 +0000

    Cellule3.2

---
+++ discovery_piscine/cell03/ex02/i_got_that.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+#!/usr/bin/env python3
+
+def main():
+    while True:
+
+        textin = input("What you gotta say? : ")
+        if textin == "STOP":
+            break
+        while True:
+            textin = input("I got that! Anything else? : ")
+            if textin == "STOP":
+                break
+        if textin == "STOP":
+            break
+        
+if __name__ == "__main__":
+    main()
+
+    
